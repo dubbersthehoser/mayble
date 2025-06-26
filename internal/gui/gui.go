d58fbfc8bf37f3962d57dae6fa73776422db3935
dubbersthehoser <dubbersthehoser@hotmail.com>
2025-06-26 01:40:41 -0700

    I made a hack for coloured icons :D

--- internal/gui/gui.go
+++ internal/gui/gui.go
@@ -2,7 +2,9 @@ package gui
 
 import (
 	//"log"
-	//"image/color"
+	"image/color"
+	//"log"
+	"fmt"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -10,11 +12,86 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/theme"
 	//"fyne.io/fyne/v2/canvas"
 
 	myapp "github.com/dubbersthehoser/mayble/internal/app"
 )
 
+type CustomIcon struct {
+	MyContent []byte
+	MyName string
+}
+func (c *CustomIcon) Content() []byte {
+	return c.MyContent
+}
+func (c *CustomIcon) Name() string {
+	return c.MyName
+}
+
+
+func ChangeSVGIconFG(svg []byte, c color.RGBA) {
+	red := fmt.Sprintf("%02x", c.R)
+	green := fmt.Sprintf("%02x", c.G)
+	blue := fmt.Sprintf("%02x", c.B)
+	valueStart := -1
+	for i, c := range svg {
+		if c == '"' && valueStart == -1 {
+			valueStart = i+2
+		} else if c == '"' && valueStart > -1 {
+			value := svg[valueStart:i]
+			if string(value) == "f3f3f3" {
+				r := value[0:2]
+				g := value[2:4]
+				b := value[4:6]
+				r[0] = red[0]
+				r[1] = red[1]
+				g[0] = green[0]
+				g[1] = green[1]
+				b[0] = blue[0]
+				b[1] = blue[1]
+			}
+			valueStart = -1
+		}
+	}
+}
+
+func NewDeleteIcon() fyne.Resource {
+	icon := theme.DeleteIcon()
+	data := icon.Content()
+	ChangeSVGIconFG(data, color.RGBA{0xff, 0x00, 0x00, 0x00})
+	myIcon := &CustomIcon{
+		MyName: "MyDeleteIcon",
+		MyContent: data,
+	}
+	return myIcon
+}
+func NewEditIcon() fyne.Resource {
+	data := theme.DocumentCreateIcon().Content()
+	ChangeSVGIconFG(data, color.RGBA{0x00, 0xff, 0x00, 0x00})
+	myIcon := &CustomIcon{
+		MyName: "MyEditIcon",
+		MyContent: data,
+	}
+	return myIcon
+}
+
+func NewBooksHeader() *fyne.Container {
+	fields := []fyne.CanvasObject{
+		widget.NewButtonWithIcon("Title", theme.MoveDownIcon(), func(){return}),
+		widget.NewLabel("Author"),
+		widget.NewLabel("Genre"),
+		widget.NewLabel("Ratting"),
+		widget.NewLabel("On Loan"),
+		container.New(layout.NewGridLayout(3), 
+			widget.NewLabel("Actions"),
+			widget.NewButtonWithIcon("", theme.ContentAddIcon(), func(){return}),
+			widget.NewButtonWithIcon("", theme.DocumentSaveIcon(), func(){return}),
+		),
+	}
+
+	return container.New(layout.NewGridLayout(len(fields)), fields...)
+}
 
 func NewBookEntry() *fyne.Container {
 	fields := []fyne.CanvasObject{
@@ -22,13 +99,16 @@ func NewBookEntry() *fyne.Container {
 		widget.NewEntry(),
 		widget.NewEntry(),
 		widget.NewSelectWithData([]string{"TBR", "⭐", "⭐⭐", "⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"}, binding.NewString()),
-		widget.NewEntry(),
-	}
+		widget.NewButtonWithIcon("", theme.CheckButtonIcon(), func(){return}),
+		container.New(layout.NewGridLayout(2),
+			widget.NewButtonWithIcon("", NewEditIcon(), func(){return}),
+			widget.NewButtonWithIcon("", NewDeleteIcon(), func(){return}),
+		)}
 	fields[0].(*widget.Entry).SetPlaceHolder("Title")
 	fields[1].(*widget.Entry).SetPlaceHolder("Author")
 	fields[2].(*widget.Entry).SetPlaceHolder("Genre")
 	fields[3].(*widget.Select).PlaceHolder = "Ratting"
-	fields[4].(*widget.Entry).SetPlaceHolder("Lounded")
+	//fields[4].(*widget.Entry).SetPlaceHolder("Lounded")
 
 	return container.New(layout.NewGridLayout(len(fields)), fields...)
 
@@ -39,11 +119,13 @@ func Run() {
 	a := app.New()
 	window := a.NewWindow(myapp.AppName)
 
+	header  := NewBooksHeader()
 	entry_1 := NewBookEntry()
 	entry_2 := NewBookEntry()
-	content := container.New(layout.NewVBoxLayout(), entry_1, entry_2)
+	content := container.New(layout.NewVBoxLayout(),header, entry_1, entry_2)
+	scroll := container.NewVScroll(content)
 
-	window.SetContent(content)
+	window.SetContent(scroll)
 	window.ShowAndRun()
 	
 }
