cdb739294a00cddbf8887dd51e7490cb70c0e355
dubbersthehoser <dubbersthehoser@hotmail.com>
2025-06-27 01:05:47 -0700

    A working edit button

--- internal/gui/gui.go
+++ internal/gui/gui.go
@@ -13,7 +13,7 @@ import (
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/theme"
-	//"fyne.io/fyne/v2/canvas"
+	_ "fyne.io/fyne/v2/canvas"
 
 	myapp "github.com/dubbersthehoser/mayble/internal/app"
 )
@@ -30,47 +30,55 @@ func (c *CustomIcon) Name() string {
 }
 
 
-func ChangeSVGIconFG(svg []byte, c color.RGBA) {
+func ChangeSVGIconFG(svg []byte, c color.RGBA) []byte {
+	data := []byte{}
 	red := fmt.Sprintf("%02x", c.R)
 	green := fmt.Sprintf("%02x", c.G)
 	blue := fmt.Sprintf("%02x", c.B)
 	valueStart := -1
 	for i, c := range svg {
 		if c == '"' && valueStart == -1 {
-			valueStart = i+2
+			valueStart = i+1
+			data = append(data, c)
 		} else if c == '"' && valueStart > -1 {
 			value := svg[valueStart:i]
-			if string(value) == "f3f3f3" {
-				r := value[0:2]
-				g := value[2:4]
-				b := value[4:6]
-				r[0] = red[0]
-				r[1] = red[1]
-				g[0] = green[0]
-				g[1] = green[1]
-				b[0] = blue[0]
-				b[1] = blue[1]
+			if string(value) == "#f3f3f3" {
+				data = append(data, '#')
+				data = append(data, red[0])
+				data = append(data, red[1])
+				data = append(data, green[0])
+				data = append(data, green[1])
+				data = append(data, blue[0])
+				data = append(data, blue[1])
+			} else {
+				for _, c := range value {
+					data = append(data, c)
+				}
 			}
 			valueStart = -1
 		}
+		if valueStart == -1 {
+			data = append(data, c)
+		}
 	}
+	return data
 }
 
 func NewDeleteIcon() fyne.Resource {
 	icon := theme.DeleteIcon()
 	data := icon.Content()
-	ChangeSVGIconFG(data, color.RGBA{0xff, 0x00, 0x00, 0x00})
+	data = ChangeSVGIconFG(data, color.RGBA{0xff, 0x00, 0x00, 0x00})
 	myIcon := &CustomIcon{
 		MyName: "MyDeleteIcon",
 		MyContent: data,
 	}
 	return myIcon
 }
-func NewEditIcon() fyne.Resource {
+func NewEditIcon(color color.RGBA, name string) fyne.Resource {
 	data := theme.DocumentCreateIcon().Content()
-	ChangeSVGIconFG(data, color.RGBA{0x00, 0x00, 0xff, 0x00})
+	data = ChangeSVGIconFG(data, color)
 	myIcon := &CustomIcon{
-		MyName: "MyEditIcon",
+		MyName: name,
 		MyContent: data,
 	}
 	return myIcon
@@ -94,27 +102,104 @@ func NewBooksHeader() *fyne.Container {
 	return container.New(layout.NewGridLayout(len(fields)), fields...)
 }
 
+
+type BookEntryWidget struct {
+	widget.BaseWidget
+	object fyne.CanvasObject
+}
+func (b *BookEntryWidget)CreateRenderer() fyne.WidgetRenderer {
+	if b == nil {
+		return nil
+	}
+	if w, ok := b.object.(fyne.Widget); ok {
+		return w.CreateRenderer()
+	}
+	return widget.NewSimpleRenderer(b.object)
+}
+func NewBookEntryWidget(o fyne.CanvasObject) *BookEntryWidget {
+	b := &BookEntryWidget{object: o}
+	b.ExtendBaseWidget(b)
+	return b
+}
+	
+
 func NewBookEntry() *fyne.Container {
+
+	bookTitle := binding.NewString()
+	bookTitle.Set("The Cat")
+
+	titleLabel := widget.NewLabelWithData(bookTitle)
+	titleLabel.Show()
+
+	titleEntry :=  widget.NewEntry()
+	authorEntry := widget.NewEntry()
+	genreEntry :=  widget.NewEntry()
+	rattingsEntry := widget.NewSelectWithData([]string{"TBR", "⭐", "⭐⭐", "⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"}, binding.NewString())
+
+	titleEntry.Bind(bookTitle)
+
+	titleEntry.Disable()
+	titleEntry.Hide()
+	authorEntry.Disable()
+	genreEntry.Disable()
+	rattingsEntry.Disable()
+
+
+	green := color.RGBA{0x00, 0xff, 0x00, 0xff}
+	editEnabledBtn := widget.NewButtonWithIcon("", NewEditIcon(green, "EditEnable"), nil)
+	
+	editDisableBtn := widget.NewButtonWithIcon("", theme.DocumentCreateIcon(), nil)
+
+	editEnabledBtn.Hide()
+
+	editBtn := container.New(layout.NewStackLayout(), editEnabledBtn, editDisableBtn)
+
+	editEnabledBtn.OnTapped = func() {
+		titleEntry.Disable()
+		authorEntry.Disable()
+		genreEntry.Disable()
+		rattingsEntry.Disable()
+
+		titleLabel.Show()
+		titleEntry.Hide()
+
+		editDisableBtn.Show()
+		editEnabledBtn.Hide()
+	}
+	editDisableBtn.OnTapped = func() {
+		titleEntry.Enable()
+		authorEntry.Enable()
+		genreEntry.Enable()
+		rattingsEntry.Enable()
+
+		titleLabel.Hide()
+		titleEntry.Show()
+
+		editDisableBtn.Hide()
+		editEnabledBtn.Show()
+	}
+
+
 	fields := []fyne.CanvasObject{
-		widget.NewEntry(),
-		widget.NewEntry(),
-		widget.NewEntry(),
-		widget.NewSelectWithData([]string{"TBR", "⭐", "⭐⭐", "⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"}, binding.NewString()),
+		container.New(layout.NewStackLayout(), titleEntry, titleLabel),
+		authorEntry,
+		genreEntry,
+		rattingsEntry,
 		widget.NewButtonWithIcon("", theme.CheckButtonIcon(), func(){return}),
 		container.New(layout.NewGridLayout(2),
-			widget.NewButtonWithIcon("", NewEditIcon(), func(){return}),
-			widget.NewButtonWithIcon("", NewDeleteIcon(), func(){return}),
-		)}
-	fields[0].(*widget.Entry).SetPlaceHolder("")
-	fields[1].(*widget.Entry).SetPlaceHolder("")
-	fields[2].(*widget.Entry).SetPlaceHolder("")
+			editBtn,
+			widget.NewButtonWithIcon("", NewDeleteIcon(), func(){return})),
+	}
 	fields[3].(*widget.Select).PlaceHolder = "TBR"
-	//fields[4].(*widget.Entry).SetPlaceHolder("Lounded")
 
 	return container.New(layout.NewGridLayout(len(fields)), fields...)
 }
 
 func Run() {
+	//data := theme.DocumentCreateIcon().Content()
+	//fmt.Println(string(data))
+	//data = ChangeSVGIconFG(data, color.RGBA{0xff, 0x00, 0x00, 0x00})
+	//fmt.Println(string(data))
 	a := app.New()
 	window := a.NewWindow(myapp.AppName)
 
