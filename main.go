859730a2ab672c3bcb7b948af5fe2e61d5a568b4
dubbersthehoser <dubbersthehoser@hotmail.com>
2026-06-22 19:00:50 -0700

    today's work

--- main.go
+++ main.go
@@ -13,18 +13,23 @@ import (
 	"github.com/dubbersthehoser/mayble/internal/viewmodel1"
 )
 
-func fatalLaunch(err error) {
+func fatalLaunch(w fyne.Window, err error) {
 	// Todo: create window to display fatal error.
 	log.Fatal(err)
+	body := view.NewFatal("Fatal", "Failed to launch application.", err.Error())
+
+	w.SetContent(body)
 }
 
 func main() {
 
 	appName := "mayble"
+	a := app.NewWithID("com.dubbersthehoser." + appName)
+	window := a.NewWindow(appName)
 
 	cfgPath, err := config.GetDefaultConfigFile(appName)
 	if err != nil {
-		fatalLaunch(err)
+		fatalLaunch(window, err)
 		return
 	}
 	cfg, err := config.Load(cfgPath)
@@ -32,31 +37,38 @@ func main() {
 	case errors.Is(err, config.ErrIsOldConfig):
 		cfg, err = config.Migrate(cfgPath, appName)
 		if err != nil {
-			fatalLaunch(err)
+			fatalLaunch(window, err)
 			return
 		}
 
 	case errors.Is(err, os.ErrNotExist):
 		cfg, err = config.NewConfigWithDefaults(appName)
 		if err != nil {
-			fatalLaunch(err)
+			fatalLaunch(window, err)
 			return
 		}
 
 	case err != nil:
-		fatalLaunch(err)
+		fatalLaunch(window, err)
 		return
 	}
 	defer cfg.Save()
 
-	a := app.NewWithID("com.dubbersthehoser." + appName)
-	window := a.NewWindow(appName)
 
 	// Todo: add window size to config.
 	window.Resize(fyne.NewSize(900, 600))
 	window.CenterOnScreen()
 	window.SetMaster()
 
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println(err)
+			// This code maybe bad for fyne. Needs testing.
+			// And the err.(string) most likely an bad idea.
+			body := view.NewFatal("Crash", "Unexpected crash.", err.(string))
+			window.SetContent(body)
+		}
+	}()
 
 	vm := viewmodel.NewWindow(cfg)
 	f := view.NewFyne(a, window)
