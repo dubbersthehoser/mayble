c714e9ad2a9e1c02a5545ba466771592883234ac
dubbersthehoser <dubbersthehoser@hotmail.com>
2026-08-02 20:14:58 -0700

    today's 'work'

---
+++ internal/viewmodel/table/table.go
@@ -0,0 +1,14 @@
+package table
+
+import (
+	
+	//"github.com/dubbersthehoser/mayble/internal/models"
+	"github.com/dubbersthehoser/mayble/internal/app"
+
+)
+
+type Table struct {
+	service *app.Service
+	data [][]string
+}
+
