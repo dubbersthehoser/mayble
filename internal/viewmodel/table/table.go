ba78a1eb4a6286a19df105f03f569242d29ccf89
dubbersthehoser <dubbersthehoser@hotmail.com>
2026-08-03 22:37:46 -0700

    added get to table

--- internal/viewmodel/table/table.go
+++ internal/viewmodel/table/table.go
@@ -1,14 +1,29 @@
 package table
 
 import (
-	
+	"errors"
 	//"github.com/dubbersthehoser/mayble/internal/models"
 	"github.com/dubbersthehoser/mayble/internal/app"
 
 )
 
+type Point struct {
+	Col int
+	Row int
+}
+
 type Table struct {
 	service *app.Service
 	data [][]string
+}
+
+func (t *Table) Get(p Point) (string, error) {
+	if p.Row >= len(t.data) || p.Row > 0 {
+		return "", errors.New("row point out of range")
+	}
+	if p.Col >= len(t.data[p.Row]) || p.Col < 0 {
+		return "", errors.New("column point out of range")
+	}
+	return t.data[p.Row][p.Col], nil
 }
 
